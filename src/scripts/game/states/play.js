1ba0809e4698b64a5936e6889d13a56693ffaf2a
jenca <lajencaku@gmail.com>
2014-05-18 15:17:47 -0700

    part 3

--- src/scripts/game/states/play.js
+++ src/scripts/game/states/play.js
@@ -5,7 +5,7 @@ function Play() { }
 Play.prototype = {
     create: function() {
         this.game.physics.startSystem(Phaser.Physics.ARCADE);
-        this.game.physics.arcade.gravity.y = 500;
+        this.game.physics.arcade.gravity.y = 1200;
 
         this.background = this.game.add.sprite(0, 0, 'background');
 
@@ -14,6 +14,11 @@ Play.prototype = {
 
         this.ground = new Ground(this.game, 0, 400, 335, 112);
         this.game.add.existing(this.ground);
+
+        this.game.input.keyboard.addKeyCapture([Phaser.Keyboard.SPACEBAR]);
+        var flapkey = this.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+        flapkey.onDown.add(this.bird.flap, this.bird);
+        this.input.onDown.add(this.bird.flap, this.bird);
     },
     update: function() {
         this.game.physics.arcade.collide(this.bird, this.ground);
